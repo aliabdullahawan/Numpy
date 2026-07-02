281d3e31c39464a3b14807a111b4bb62eabdae0e
aliabdullahawan <aliabdullahawan.2003@gmail.com>
2026-07-02 14:41:52 +0500

    Lecture 04

--- lec4.py
+++ lec4.py
@@ -1,6 +1,18 @@
 import numpy as np 
 
 data = np.array([112.32, 139.527, 128.009, 146.805,157.172])
+print(data)
 
 data_sqrt = np.sqrt(data)
+print(data_sqrt)
 data_round = np.round(data)
+print(data_round)
+data_floor = np.floor(data)
+print(data_floor)
+data_ceil = np.ceil(data)
+print(data_ceil)
+
+print(np.pi)
+
+
+
